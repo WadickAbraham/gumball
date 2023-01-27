5e4a92aaf2625efd7f58f0f6ed1a981e78138dc6
WadickAbraham <123760260+WadickAbraham@users.noreply.github.com>
2023-01-27 18:12:31 +0000

    i did  this in cmu cs  academy

---
+++ project1.py
@@ -0,0 +1,12 @@
+# i will be using functions Rect and Circle to make a smiley face
+Circle(200,200,200)
+Rect(50,75,100,100, fill ='yellow')
+Rect(250,75,100,100, fill='yellow')
+Rect(150,300,100,10 , fill='yellow')
+Rect(150,200,10,100, fill='yellow')
+Rect(250,200,10,110, fill='yellow')
+Rect(95,100,25,25, fill='blue')
+Rect(275,100,25,25, fill='blue')
+Rect(175,170,50,75,fill='yellow')
+Line(80,60,170,70,fill='green')
+Line(250,70,300,40,fill='green')
